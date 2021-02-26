29014ca8f436e3358b259cf9a72aa43a03d6b00d
= <ogzhnaydin06@gmail.com>
2021-02-26 22:05:37 +0300

    new items added

--- src/productList.js
+++ src/productList.js
@@ -162,5 +162,110 @@ export const productList = [
             },
         ]
     },
+    {
+        "İsim":"Kızıl Rüya",
+        "Fiyat": 385,
+        "Etiket": "akik,ametist,ay taşı",
+        "Renk": "kırmızı,yavruağzı,beyaz",
+        "Çeşit": "taş",
+        "Açıklama": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci animi architecto aperiam, tempore est sit enim vitae expedita excepturi sed dolore dicta doloremque repellat quasi modi dolor voluptas in inventore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci animi architecto aperiam, tempore est sit enim vitae expedita excepturi sed dolore dicta doloremque repellat quasi modi dolor voluptas in inventore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci animi architecto aperiam, tempore est sit enim vitae expedita excepturi sed dolore dicta doloremque repellat quasi modi dolor voluptas in inventore.",
+
+        "SocialLinks":[
+            {
+                "title": "letgo",
+                "imglink" : "https://svgshare.com/i/UMJ.svg",
+                "link": "https://www.letgo.com/tr-tr",
+            },
+            {
+                "title": "instagram",
+                "imglink" : "https://svgshare.com/i/UL_.svg",
+                "link": "https://www.instagram.com/",
+            }
+        ],
+        "Link": [
+            {
+                "link":"https://picsum.photos/id/301/270",
+                "title":"Title"
+            },
+            {
+                "link":"https://picsum.photos/id/302/270",
+                "title":"Title"
+            },
+            {
+                "link":"https://picsum.photos/id/362/270",
+                "title":"Title"
+            },
+        ]
+    },
+    {
+        "İsim":"Toprak",
+        "Fiyat": 100,
+        "Etiket": "iyolit,kaplan gözü",
+        "Renk": "mor,kahverengi,beyaz",
+        "Çeşit": "taş,deri",
+        "Açıklama": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci animi architecto aperiam, tempore est sit enim vitae expedita excepturi sed dolore dicta doloremque repellat quasi modi dolor voluptas in inventore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci animi architecto aperiam, tempore est sit enim vitae expedita excepturi sed dolore dicta doloremque repellat quasi modi dolor voluptas in inventore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci animi architecto aperiam, tempore est sit enim vitae expedita excepturi sed dolore dicta doloremque repellat quasi modi dolor voluptas in inventore.",
+
+        "SocialLinks":[
+            {
+                "title": "letgo",
+                "imglink" : "https://svgshare.com/i/UMJ.svg",
+                "link": "https://www.letgo.com/tr-tr",
+            },
+            {
+                "title": "instagram",
+                "imglink" : "https://svgshare.com/i/UL_.svg",
+                "link": "https://www.instagram.com/",
+            }
+        ],
+        "Link": [
+            {
+                "link":"https://picsum.photos/id/222/270",
+                "title":"Title"
+            },
+            {
+                "link":"https://picsum.photos/id/111/270",
+                "title":"Title"
+            },
+            {
+                "link":"https://picsum.photos/id/403/270",
+                "title":"Title"
+            },
+        ]
+    },
+    {
+        "İsim":"Tasarım Abiye Çanta",
+        "Fiyat": 120,
+        "Etiket": "cüzdan,lapis,obsidyen",
+        "Renk": "siyah,mor,gri",
+        "Çeşit": "taş,keçe",
+        "Açıklama": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci animi architecto aperiam, tempore est sit enim vitae expedita excepturi sed dolore dicta doloremque repellat quasi modi dolor voluptas in inventore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci animi architecto aperiam, tempore est sit enim vitae expedita excepturi sed dolore dicta doloremque repellat quasi modi dolor voluptas in inventore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci animi architecto aperiam, tempore est sit enim vitae expedita excepturi sed dolore dicta doloremque repellat quasi modi dolor voluptas in inventore.",
+
+        "SocialLinks":[
+            {
+                "title": "letgo",
+                "imglink" : "https://svgshare.com/i/UMJ.svg",
+                "link": "https://www.letgo.com/tr-tr",
+            },
+            {
+                "title": "instagram",
+                "imglink" : "https://svgshare.com/i/UL_.svg",
+                "link": "https://www.instagram.com/",
+            }
+        ],
+        "Link": [
+            {
+                "link":"https://picsum.photos/id/408/270",
+                "title":"Title"
+            },
+            {
+                "link":"https://picsum.photos/id/295/270",
+                "title":"Title"
+            },
+            {
+                "link":"https://picsum.photos/id/404/270",
+                "title":"Title"
+            },
+        ]
+    },
 ]
 
